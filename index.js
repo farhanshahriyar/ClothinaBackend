6ab042c593139b8dd2153627f5ef07174e96ffb8
farhanshahriyar <farhan.abir9999@gmail.com>
2023-01-19 21:25:02 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const app = express();
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+const userRoute = require("./routes/user")
+
+
+// env config
+dotenv.config();
+// DB Settings
+mongoose.set('strictQuery', true);
+// mongodbURL
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log("Entered Warehouse Seccessfully!🔫")) 
+.catch((err)=> {
+    console.log(err);
+});
+
+app.use(express.json());
+app.use("/api/users", userRoute);
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Backend server is running bruh🔥")
+})
