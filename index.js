30f7e3ef42a24802b47a9211c3e044a34e5de252
farhanshahriyar <farhan.abir9999@gmail.com>
2023-01-26 19:24:34 +0600

    added cart order and product functionalities

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 const userRoute = require("./routes/user")
 const authRoute = require("./routes/auth")
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
 
 
 // env config
@@ -20,6 +22,8 @@ mongoose.connect(process.env.MONGO_URL)
 app.use(express.json());
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Backend server is running bruh🔥")
