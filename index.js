f6d536c4201a0478755b042cbb3df143bf115d45
farhanshahriyar <farhan.abir9999@gmail.com>
2023-01-26 20:10:42 +0600

    added stripe functionalities

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const userRoute = require("./routes/user")
 const authRoute = require("./routes/auth")
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe")
+const cors = require("cors")
 
 
 // env config
@@ -19,11 +22,15 @@ mongoose.connect(process.env.MONGO_URL)
     console.log(err);
 });
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
+
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Backend server is running bruh🔥")
--- routes/order.js
+++ routes/order.js
@@ -3,8 +3,7 @@ const { verifyToken,
     verifyTokenAndAuthorization,
   verifyTokenAndAdmin } = require("./verifyToke")
 
-  
-const router = require("express").Router();
+
 
 //CREATE
 
